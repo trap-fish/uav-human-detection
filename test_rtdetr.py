4be9cce6386a327dd615e5c98b46dd1bf11a84b6
trap-fish <fishfoshfish@outlook.com>
2025-02-19 16:14:20 +0800

    added some simple changes such as new file paths etc

--- test_rtdetr.py
+++ test_rtdetr.py
@@ -13,16 +13,16 @@ wkdir = "/media/citi-ai/matthew/uav-human-detection"
 # model_path = "/media/citi-ai/matthew/mot-detection-training/results/exp1_yolo11n_results_hituav/yolo11n_cosine/weights/best.pt"
 # model_path = "/media/citi-ai/matthew/mot-detection-training/runs/detect/train31/weights/best.pt"
 # model_path = "results/exp1_training_results/exp_5_yolo11n_SGD_lr0.001/weights/best.pt"
-model_rltv_path = "models/exp_1_yolo11n_SGD_lr0.01_Okutama_finetuned.pt"
-model_rltv_path = "runs/detect/train31/weights/best.pt" # train28 is trained on visdrone, 31 is combined visdrone/okutama
-
+# model_rltv_path = "models/exp_1_yolo11n_SGD_lr0.01_Okutama_finetuned.pt"
+# model_rltv_path = "runs/detect/train31/weights/best.pt" # train28 is trained on visdrone, 31 is combined visdrone/okutama
+model_rltv_path = "/media/citi-ai/matthew/uav-human-detection/runs/detect/train34/weights/best.pt"
 model_path = os.path.join(wkdir, model_rltv_path)
 model = YOLO(model_path)
 
 #data_path = "human-det.yaml"
 #data_path = "Okutama.yaml"
 #data_path = "hit-uav.yaml"
-data_rltv_path = "data_files/Okutama.yaml" #niicu_test_rgb.yaml"
+data_rltv_path = "data_files/VisDrone.yaml"
 dataset = data_rltv_path.split(".", maxsplit=1)[0].split("/")[-1].lower().replace("-", "_")
 data_path = os.path.join(wkdir, data_rltv_path)
 
