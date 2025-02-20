47a6a67c933601f70fe54f5601cdb451678da273
trap-fish <fishfoshfish@outlook.com>
2025-02-20 14:20:21 +0800

    added additional formats to export script

--- utils/export_model.py
+++ utils/export_model.py
@@ -7,9 +7,10 @@ model_rltv_path = "/media/citi-ai/matthew/uav-human-detection/runs/detect/train3
 #model_rltv_path = "yolov8n.pt"
 model_path = os.path.join(wkdir, model_rltv_path)
 model = YOLO(model_path)
-
+imgsz = (640, 640)
 data_path = os.path.join(wkdir, "data_files/VisDrone.yaml")
 
 #model.export(format='tflite', int8=True, device='cpu', data=data_path)
-model.export(format='tflite', half=True, device='cpu', data=data_path)
-
+#model.export(format='tflite', int8=True, device='cpu', data=data_path, imgsz=imgsz)
+#model.export(format='ncnn', half=True, imgsz=imgsz)
+model.export(format='onnx', imgsz=imgsz, half=True, simplify=True, nms=True, conf=0.4)
