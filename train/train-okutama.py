edd36c6d22be85c3f6ec6ff8f2e225b857d64801
trap-fish <fishfoshfish@outlook.com>
2025-03-24 17:41:53 +0800

    amended to train yolo11

--- train/train-okutama.py
+++ train/train-okutama.py
@@ -13,18 +13,20 @@ datasets =  ["VisDrone.yaml"]
 # model paths
 model_dir_pth = os.path.join(wkdir, "models/")
 yolov5n_pth = "yolov5nu.pt"
+yolov8n_pth = "yolov8n.pt"
 yolo11n_pth = "yolo11n.pt"
 yolo11s_pth = "yolo11s.pt"
-yolo11p2_path = "yolo11-p2.yaml"
-yolo5p2_path = "yolo5-p2.yaml"
+#yolo11p2_path = "yolo11-p2.yaml"
+#yolo5p2_path = "yolo5-p2.yaml"
 
 
 model_dir = {
-    "yolo5n": {"type": "yolo", "path": yolov5n_pth},
+    #"yolo5n": {"type": "yolo", "path": yolov5n_pth},
     #"yolop2n": {"type": "yolop2", "path": yolo5p2_path}
     #"yolop2n": {"type": "yolop2", "path": yolo11p2_path},
-    # "yolo11n": {"type": "yolo", "path": yolo11n_pth},
-    # "yolo11s": {"type": "yolo", "path": yolo11s_pth},
+    #"yolov8n": {"type": "yolo", "path": yolov8n_pth},
+    "yolo11n": {"type": "yolo", "path": yolo11n_pth},
+    "yolo11s": {"type": "yolo", "path": yolo11s_pth},
     # "yolop2s": {"type": "yolop2", "path": yolo11p2_path},
 }
 
@@ -32,12 +34,12 @@ model_dir = {
 experiments = [
     {"optimizer": "SGD", "lr": 0.01, "freeze": None, "cos_lr": True},
     {"optimizer": "SGD", "lr": 0.01, "freeze": 10, "cos_lr": True},
-    {"optimizer": "SGD", "lr": 0.01, "freeze": 23, "cos_lr": True},
+    {"optimizer": "SGD", "lr": 0.01, "freeze": 22, "cos_lr": True},
     {"optimizer": "SGD", "lr": 0.01, "freeze": None, "cos_lr": False},
     {"optimizer": "SGD", "lr": 0.001, "freeze": None, "cos_lr": False},
     {"optimizer": "SGD", "lr": 0.0005, "freeze": None, "cos_lr": False},
     {"optimizer": "SGD", "lr": 0.01, "freeze": 10, "cos_lr": False},
-    {"optimizer": "SGD", "lr": 0.01, "freeze": 23, "cos_lr": False},
+    {"optimizer": "SGD", "lr": 0.01, "freeze": 22, "cos_lr": False},
     {"optimizer": "SGD", "lr": 0.001, "freeze": 10, "cos_lr": False},
     {"optimizer": "SGD", "lr": 0.0005, "freeze": 10, "cos_lr": False},
 ]
