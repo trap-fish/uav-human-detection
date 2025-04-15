c7d62f9e61d07e3d4c59e347044e444d75dacf90
trap-fish <fishfoshfish@outlook.com>
2025-04-15 18:31:35 +0800

    added augmentation params

--- train/train-okutama.py
+++ train/train-okutama.py
@@ -18,41 +18,50 @@ yolov8n_pth = "yolov8n.pt"
 yolo11n_pth = "yolo11n.pt"
 yolo11s_pth = "yolo11s.pt"
 rtdetr_pth = "rtdetr-l.pt"
-#yolo11p2_path = "yolo11-p2.yaml"
+yolo11p2_path = "yolo11-p2.yaml"
 #yolo5p2_path = "yolo5-p2.yaml"
 
 
 model_dir = {
     # "yolo5n": {"type": "yolo", "path": yolov5n_pth},
-    # "yolo5s": {"type": "yolo", "path": yolo5s_pth},
+    "yolo5s": {"type": "yolo", "path": yolo5s_pth},
     #"yolop2n": {"type": "yolop2", "path": yolo5p2_path}
     #"yolop2n": {"type": "yolop2", "path": yolo11p2_path},
     #"yolov8n": {"type": "yolo", "path": yolov8n_pth},
     # "yolo11n": {"type": "yolo", "path": yolo11n_pth},
-    "yolo11s": {"type": "yolo", "path": yolo11s_pth},
+    # "yolo11s": {"type": "yolo", "path": yolo11s_pth},
     # "yolop2s": {"type": "yolop2", "path": yolo11p2_path},
     # "rtdetrl": {"type": "rtdetr", "path": rtdetr_pth},
 }
 
 # Define experiments: model, optimizer, and learning rate combinations
+# 10 for backbone (9 for rtdetr), 23 for yolo5, 22 for yolo11, 28 for 11p2, 27 rtdet
+freeze = [10, 23]
 experiments = [
     {"optimizer": "SGD", "lr": 0.01, "freeze": None, "cos_lr": True},
-    {"optimizer": "SGD", "lr": 0.01, "freeze": 9, "cos_lr": True}, # 10 for yolo, 9 for rtdetr
-    {"optimizer": "SGD", "lr": 0.01, "freeze": 27, "cos_lr": True}, # 22 for yolo11, 27 for rtdet
+    {"optimizer": "SGD", "lr": 0.01, "freeze": freeze[0], "cos_lr": True},
+    {"optimizer": "SGD", "lr": 0.01, "freeze": freeze[1], "cos_lr": True}, 
     {"optimizer": "SGD", "lr": 0.01, "freeze": None, "cos_lr": False},
     {"optimizer": "SGD", "lr": 0.001, "freeze": None, "cos_lr": False},
     {"optimizer": "SGD", "lr": 0.0005, "freeze": None, "cos_lr": False},
-    {"optimizer": "SGD", "lr": 0.01, "freeze": 9, "cos_lr": False},
-    {"optimizer": "SGD", "lr": 0.01, "freeze": 27, "cos_lr": False},
-    {"optimizer": "SGD", "lr": 0.001, "freeze": 9, "cos_lr": False},
-    {"optimizer": "SGD", "lr": 0.0005, "freeze": 9, "cos_lr": False},
+    {"optimizer": "SGD", "lr": 0.01, "freeze": freeze[0], "cos_lr": False},
+    {"optimizer": "SGD", "lr": 0.01, "freeze": freeze[1], "cos_lr": False},
+    {"optimizer": "SGD", "lr": 0.001, "freeze": freeze[0], "cos_lr": False},
+    {"optimizer": "SGD", "lr": 0.0005, "freeze": freeze[0], "cos_lr": False},
 ]
 
 # Paths and parameters
 EPOCHS = 150  # Number of training epochs
 BATCH_SZ = 16
-PATIENCE = 20
+PATIENCE = 15
 IMGSZ = 640  # Image size for training
+SINGLE_CLS = True
+DEGREES=45        
+SCALE=0.9
+SHEAR=0 # 2
+PERSPECTIVE=0.0005
+MOSAIC=0.5
+MIXUP=0.5
 
 # Directory to save results
 res_rltv_dir = f"results/experiment_{expdate}"
@@ -91,6 +100,8 @@ for data_yml in datasets:
 
         # Loop through experiments
         for i, exp in enumerate(experiments):
+            if i < 4:
+                continue
             print(f"\nStarting Experiment {i + 1}: {exp}")
             params = f"{exp['optimizer']}_lr{exp['lr']}_frz{exp['freeze']}_coslr{exp['cos_lr']}"
             variation = data_path.split('/')[-1].split(".")[0]
@@ -109,7 +120,14 @@ for data_yml in datasets:
                 name=exp_name,
                 freeze=exp["freeze"],
                 cos_lr=exp["cos_lr"],
-                augment=True
+                augment=True,
+                single_cls=SINGLE_CLS,
+                # degrees=DEGREES,
+                # scale=SCALE,
+                # shear=SHEAR,
+                # perspective=PERSPECTIVE,
+                # mosaic=MOSAIC,
+                # mixup=MIXUP
             )
 
             print(f"Experiment {i + 1} completed! Results saved in {results_dir}/{exp_name}")
